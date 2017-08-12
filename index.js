723345dc5f7c8d61965c0d91755f1c91ea99bc4f
rom <penaflor024@gmail.com>
2017-08-12 15:26:05 +0800

    in progress

--- index.js
+++ index.js
@@ -85,7 +85,7 @@ function receivedMessage(event) {
         sendGenericMessage(senderID);
         break;
 
-      case 'generic':
+      case 'menu':
         sendMenu(senderID);
         break;
       default:
